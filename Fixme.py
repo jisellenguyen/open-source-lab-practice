c88d6541fa143fbf241a92d99ea7660290b314b1
Philo Judson <philojejudson@gmail.com>
2026-09-18 11:12:16 -0700

    python fixes 3

--- Fixme.py
+++ Fixme.py
@@ -4,7 +4,4 @@ def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
     '''
-    result=[]
-    for i in range(0,n+1/2):
-        result +=  2*i
-    return result
+    return list(range(0,n+1,2)
